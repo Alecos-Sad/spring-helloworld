6194f3c01840af41a03dc980c0e413c4b49a455c
Sadonikov Alexander <82374963+Alecos-Sad@users.noreply.github.com>
2022-12-04 18:03:41 +0300

    Add files via upload

---
+++ src/main/java/com/kata/sadovnick/SadovnickApplication.java
@@ -0,0 +1,13 @@
+package com.kata.sadovnick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadovnickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SadovnickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kata/sadovnick/SadovnickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.sadovnick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SadovnickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
